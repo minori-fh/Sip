4c20056e19798a953821702cc7787c25dacb1044
Minori Hashimoto <minori.fh@gmail.com>
2019-06-11 10:13:40 -0700

    add dropdown button and links for location and price

--- testJS.js
+++ testJS.js
@@ -1,20 +1,30 @@
 // $(document).ready(function() {
 
-// $("#loading-page").hide()
-// $("#main-page").show()
+// $('#loading-page').show(0).delay(3000).hide(0);
+// $('#main-page').hide(0).delay(3000).show(0);
 
 // $("#array-coffee").hide()
 // $("#array-drink").hide()
 // $("#google-maps").hide()
 // $("#second-user-input-page").hide();
 
-// //Event handler: when user picks alcohol
+
+// //Declare variables
+// var alcohol = "";
+// var coffee = "";
+
+// // //Event handler: when user picks alcohol
 // $("#user-pick-alc").on("click",function() {
+//     alcohol = 1
 //     $("#main-page").hide()
 //     $("#second-user-input-page").show()
 // });
 
-// //Event handler: when user picks coffee
+// if (alcohol === 1){
+
+// }
+
+// // //Event handler: when user picks coffee
 // $("#user-pick-coffee").on("click",function() {
 //     $("#main-page").hide()
 //     $("#second-user-input-page").show()
