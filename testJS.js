fb4d06d55b8cbd2dfcf5cf2bdfa08eb196f4ffda
Minori Hashimoto <minori.fh@gmail.com>
2019-06-11 10:41:41 -0700

    event handler on click functionality for when user picks location or ptice

--- testJS.js
+++ testJS.js
@@ -1,33 +1,50 @@
-// $(document).ready(function() {
-
-// $('#loading-page').show(0).delay(3000).hide(0);
-// $('#main-page').hide(0).delay(3000).show(0);
-
-// $("#array-coffee").hide()
-// $("#array-drink").hide()
-// $("#google-maps").hide()
-// $("#second-user-input-page").hide();
+$(document).ready(function() {
 
 
-// //Declare variables
-// var alcohol = "";
-// var coffee = "";
+$("#loading-page").show()
+$("#main-page").hide()
 
-// // //Event handler: when user picks alcohol
-// $("#user-pick-alc").on("click",function() {
-//     alcohol = 1
-//     $("#main-page").hide()
-//     $("#second-user-input-page").show()
-// });
+setTimeout(function(){$('#loading-page').hide();}, 1000);
+setTimeout(function(){$('#main-page').show();}, 1000);
 
-// if (alcohol === 1){
+$("#array-coffee").hide()
+$("#array-drink").hide()
+$("#google-maps").hide()
+$("#second-user-input-page").hide();
 
-// }
 
-// // //Event handler: when user picks coffee
-// $("#user-pick-coffee").on("click",function() {
-//     $("#main-page").hide()
-//     $("#second-user-input-page").show()
-// });
+//Declare variables
+var alcohol = 0;
+var coffee = 0;
 
-// }); //END document ready function
+//Event handler: when user picks alcohol
+$("#user-pick-alc").on("click",function() {
+    alcohol = 1;
+    coffee = 0; 
+    $("#main-page").hide()
+    $("#second-user-input-page").show()
+});
+
+
+//Event handler: when user picks coffee
+$("#user-pick-coffee").on("click",function() {
+    coffee = 1;
+    alcohol = 0; 
+    $("#main-page").hide()
+    $("#second-user-input-page").show()
+});
+
+//Event handler: when user clicks on price or location option
+$("#oneD, #twoD, #threeD, #fourD, #IR, #OR, #IS, #OS, #FH, #C, #pacificH, #P, #E, #missionD, #potreroH, #marinaD, #F, #SOMA, #FiDi, #NB").on("click",function(){
+if (coffee === 1){
+    $("#second-user-input-page").hide()
+    $("#array-coffee").show()
+}
+
+if (alcohol === 1){
+    $("#second-user-input-page").hide()
+    $("#array-drink").show()
+}
+});
+
+}); //END document ready function
