763c0d629cd5f2a2d94d76860baa61fd87c411ce
Minori Hashimoto <minori.fh@gmail.com>
2019-06-11 09:16:40 -0700

    change buttons to href link

--- testJS.js
+++ testJS.js
@@ -1,26 +1,23 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-$("#loading-page").hide()
-$("#main-page").show()
+// $("#loading-page").hide()
+// $("#main-page").show()
 
-$("#array-coffee").hide()
-$("#array-drink").hide()
-$("#google-maps").hide()
-$("#second-user-input-page").hide();
+// $("#array-coffee").hide()
+// $("#array-drink").hide()
+// $("#google-maps").hide()
+// $("#second-user-input-page").hide();
 
-//Event handler: when user picks alcohol
-$("#user-pick-alc").on("click",function() {
-    $("#main-page").hide()
-    $("#second-user-input-page").show()
-});
+// //Event handler: when user picks alcohol
+// $("#user-pick-alc").on("click",function() {
+//     $("#main-page").hide()
+//     $("#second-user-input-page").show()
+// });
 
-//Event handler: when user picks coffee
-$("#user-pick-coffee").on("click",function() {
-    $("#main-page").hide()
-    $("#second-user-input-page").show()
-});
+// //Event handler: when user picks coffee
+// $("#user-pick-coffee").on("click",function() {
+//     $("#main-page").hide()
+//     $("#second-user-input-page").show()
+// });
 
-
-
-
-}); //END document ready function
+// }); //END document ready function
