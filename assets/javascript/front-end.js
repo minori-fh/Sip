fc2e623b06e4a57f1cb52cac308230bd4eee8bd4
Derek Goldstone <derekgoldstone@yahoo.com>
2019-06-13 15:11:43 -0700

    Ammended formatting of code on front-end js.

--- assets/javascript/front-end.js
+++ assets/javascript/front-end.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
 
-$("#loading-page").show()
-$("#main-page").hide()
+$("#loading-page").show();
+$("#main-page").hide();
 
 setTimeout(function(){$('#loading-page').hide();}, 3000);
 setTimeout(function(){$('#main-page').show();}, 3000);
 
-$("#array-coffee").hide()
-$("#array-drink").hide()
-$("#google-maps").hide()
+$("#array-coffee").hide();
+$("#array-drink").hide();
+$("#google-maps").hide();
 $("#second-user-input-page").hide();
 
 
@@ -45,33 +45,33 @@ $("#alc-picture").on("click",function(e) {
 //Event handler: when user clicks on price or location option
 $("#oneD, #twoD, #threeD, #fourD, #IR, #OR, #IS, #OS, #FH, #C, #pacificH, #P, #E, #missionD, #potreroH, #marinaD, #F, #SOMA, #FiDi, #NB").on("click",function(){
 if (coffee === 1){
-    $("#second-user-input-page").hide()
-    $("#array-coffee").show()
+    $("#second-user-input-page").hide();
+    $("#array-coffee").show();
 }
 
 if (alcohol === 1){
-    $("#second-user-input-page").hide()
-    $("#array-drink").show()
+    $("#second-user-input-page").hide();
+    $("#array-drink").show();
 }
 });
 
 //Event handler: when user clicks "thank you, next"
 $("#next-button-1").on("click",function(){
-    tyNextC++
+    tyNextC++;
     console.log(tyNextC)
     if (tyNextC === 3){
-        $("#array-coffee").hide()
-        $("#google-maps").show()
+        $("#array-coffee").hide();
+        $("#google-maps").show();
 
     }
 });
 
 $("#next-button-2").on("click",function(){
-    tyNextA++
+    tyNextA++;
     console.log(tyNextA)
     if (tyNextA === 3){
-        $("#array-drink").hide()
-        $("#google-maps").show()
+        $("#array-drink").hide();
+        $("#google-maps").show();
     }
 });
 
